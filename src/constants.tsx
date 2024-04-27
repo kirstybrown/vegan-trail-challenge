248afb6821f8c7886ecfe73d7afa1024866787bd
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2024-04-27 13:38:33 +0200

    adding init structure and responsive nav-bar

---
+++ src/constants.tsx
@@ -0,0 +1,55 @@
+import { Icon } from "@iconify/react";
+
+import { SideNavItem } from "./types";
+
+export const SIDENAV_ITEMS: SideNavItem[] = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <Icon icon="lucide:home" width="24" height="24" />,
+  },
+  {
+    title: "Races",
+    path: "/races",
+    icon: <Icon icon="lucide:folder" width="24" height="24" />,
+    submenu: true,
+    subMenuItems: [
+      { title: "All", path: "/races" },
+      { title: "Mini", path: "/races/mini" },
+      { title: "Half", path: "/races/half" },
+      { title: "Marathon", path: "/races/marathon" },
+      { title: "Ultra", path: "/races/ultra" },
+    ],
+  },
+  {
+    title: "Registration",
+    path: "/registration",
+    icon: <Icon icon="lucide:mail" width="24" height="24" />,
+  },
+  {
+    title: "Info",
+    path: "/info",
+    icon: <Icon icon="lucide:settings" width="24" height="24" />,
+    submenu: true,
+    subMenuItems: [
+      { title: "Timetable", path: "/info/timetable" },
+      { title: "Rules", path: "/info/rules" },
+      { title: "Other Info", path: "/info/other-info" },
+    ],
+  },
+  {
+    title: "Gallery",
+    path: "/gallery",
+    icon: <Icon icon="lucide:help-circle" width="24" height="24" />,
+  },
+  {
+    title: "Results",
+    path: "/results",
+    icon: <Icon icon="lucide:help-circle" width="24" height="24" />,
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    icon: <Icon icon="lucide:help-circle" width="24" height="24" />,
+  },
+];
